560dc3a984c6ed292f5f71819f1342da8d06dfc9
qinwenchuang <qinwenchuang@baidu.com>
2020-02-13 03:05:08 -0800

    add develop

---
+++ test/test.cpp
@@ -0,0 +1,9 @@
+#include "test.h"
+
+int base::get_age() {
+    return data_source.age;
+}
+
+std::string base::get_name() {
+    return data_source.name;
+}
---
+++ test/test.h
@@ -0,0 +1,28 @@
+#ifndef _TEST_H_
+#define _TEST_H_
+
+#include <iostream>
+#include <string>
+
+struct data{
+    int a = 0;
+    std::string name;
+    int age = 0;
+};
+
+
+class base{
+public:
+    int get_age();
+    std::string get_name();
+
+private:
+    data data_source;
+
+};
+
+
+
+
+
+#endif
